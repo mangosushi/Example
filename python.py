b38ea1fcde1624049fbd2d8618c5e3f170b7477d
Briana Lee <mangosushi122@gmail.com>
2021-07-12 16:41:23 -0400

    adds greeting and counts to ten

---
+++ python.py
@@ -0,0 +1,10 @@
+print("Hello!\nI will now count to 10:")
+for x in range(1,11):
+    if x != 10:
+        print(str(x) + ", ", end = "")
+
+    elif x == 10:
+        print(x)
+
+
+print("Yay! I did it! :)")
